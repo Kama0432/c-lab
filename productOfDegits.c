897c6082e274088311cf48f171e768d449a4e216
Kamal <kamalmodanwal620@gmail.com>
2024-11-25 12:58:34 +0530

    Add files via upload

---
+++ productOfDegits.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+    {
+        int n, rem, pro=1;
+        printf("Enter number:");
+        scanf("%d",&n);
+        while(n>0)
+        {
+            rem=n%10;
+            pro=pro*rem;
+            n=n/10;
+        }
+        printf("\nProduct of degits=%d",pro);
+        
+     return 0;
+    }
