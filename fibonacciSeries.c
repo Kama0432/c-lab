936a2382d8655316166c8177f465c96db57a62f6
Kamal <kamalmodanwal620@gmail.com>
2024-11-27 19:37:37 +0530

    Add files via upload

---
+++ fibonacciSeries.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+  int n,i,a=0,b=1,c=0;
+  printf("Enter number of term:");
+  scanf("%d",&n);
+
+  for(i=1;i<=n;i++)
+  {
+
+    printf("%d  ",a);
+
+    c=a+b;
+    a=b;
+    b=c;
+  }
+    return 0;
+
+}
