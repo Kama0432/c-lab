10e1827690f1b342e68481da3c77c6e67493a3f5
Kamal <kamalmodanwal620@gmail.com>
2024-11-29 01:17:39 +0530

    Add files via upload

---
+++ AddMatrix.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+main()
+{
+  int a[3][3],b[3][3],c[3][3],i,j;
+
+  printf("enter the element of first matrix:\n");
+  for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+        printf("\n");
+    }
+   
+   printf("\nenter the elements of second matrix:\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            scanf("%d",&b[i][j]);
+        }
+        printf("\n");
+    }
+   
+   // addition 
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            c[i][j]=a[i][j]+b[i][j];
+        }
+    }
+     printf("\n1st matrix is:\n");
+     for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d\t",a[i][j]);
+        }
+        printf("\n");
+    }
+     
+     printf("\n2nd matrix is:\n");
+     for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d\t",b[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("sum of matrix is:\n");
+     for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d\t",c[i][j]);
+        }
+        printf("\n");
+    }
+     
+     
+ return 0;
+}
